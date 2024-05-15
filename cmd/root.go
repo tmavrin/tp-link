e45bfcb28f1cea1846c32b766255ee4dab9dca45
Tomi Mavrin <tomi.mavrin@gmail.com>
2024-05-15 19:10:38 +0200

    feat: command search inbox for match

--- cmd/root.go
+++ cmd/root.go
@@ -1,9 +1,15 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	cfgFile string
 )
 
 var rootCmd = &cobra.Command{
@@ -17,5 +23,37 @@ func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tp-link.yaml)")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		fmt.Printf("Using config file: %s\n", cfgFile)
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".tp-link")
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			fmt.Println("Config not found. Creating config with defaults at $HOME/.tp-link.yaml ")
+			viper.Set("username", "admin")
+			viper.Set("password", "admin")
+			viper.Set("host", "http://192.168.1.1")
+			home, err := os.UserHomeDir()
+			cobra.CheckErr(err)
+			err = viper.WriteConfigAs(fmt.Sprintf("%s/.tp-link.yaml", home))
+			cobra.CheckErr(err)
+		}
 	}
 }
--- cmd/sms.go
+++ cmd/sms.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/tmavrin/tp-link/client"
@@ -23,9 +24,13 @@ func init() {
 	rootCmd.AddCommand(smsCmd)
 	smsCmd.AddCommand(smsListCmd)
 	smsCmd.AddCommand(smsSendCmd)
+	smsCmd.AddCommand(smsFindCmd)
 
 	smsSendCmd.PersistentFlags().String("to", "", "phone number to send the message to")
 	smsSendCmd.PersistentFlags().String("content", "", "content of the message")
+
+	smsFindCmd.PersistentFlags().String("from", "", "phone number message was received from")
+	smsFindCmd.PersistentFlags().String("phrase", "", "phrase to look for in a message")
 }
 
 var smsListCmd = &cobra.Command{
@@ -92,5 +97,57 @@ var smsSendCmd = &cobra.Command{
 		}
 
 		fmt.Println("sms sent successfully")
+	},
+}
+
+var smsFindCmd = &cobra.Command{
+	Use:   "find",
+	Short: "Searches inbox for keyword from sender",
+	Long:  `Search inbox from sender number with message content keywords`,
+	Run: func(cmd *cobra.Command, args []string) {
+		from, err := cmd.Flags().GetString("from")
+		if err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
+		phrase, err := cmd.Flags().GetString("phrase")
+		if err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
+
+		if phrase == "" {
+			fmt.Println("--phrase must be set")
+			os.Exit(1)
+		}
+
+		if from == "" {
+			fmt.Println("--from not set, searching all messages")
+		}
+
+		c, err := client.Authenticate("http://192.168.1.1", "admin", "admin")
+		if err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
+
+		defer c.Close()
+
+		inbox, err := sms.New(c).GetInbox()
+		if err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
+
+		for _, msg := range inbox {
+			f := from == "" || from == msg.From
+			if f && strings.Contains(msg.Content, phrase) {
+				fmt.Printf("Found phrase \"%s\" in message:\n%s\nfrom: %s\n", phrase, msg.Content, msg.From)
+				os.Exit(0)
+			}
+		}
+
+		fmt.Printf("Phrase \"%s\" not found in any message\n", phrase)
+		os.Exit(1)
 	},
 }
